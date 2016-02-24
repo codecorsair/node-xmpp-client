bcc4f63552793460d2186ee76c17c28298e40857
JB <jb@codecorsair.com>
2016-02-24 12:12:45 -0500

    hacky exports

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ var Client = require('./lib/Client')
 var SASL = require('./lib/sasl')
 var core = require('node-xmpp-core')
 
-module.exports = Client
-module.exports.Client = Client
-module.exports.SASL = SASL
-
-core.exportCoreUtils(module.exports)
+var moduleExports = {}
+core.exportCoreUtils(moduleExports)
+moduleExports = Client
+moduleExports.Client = Client
+moduleExports.SASL = SASL
+moduleExports.Element = moduleExports.Element
+module.exports = moduleExports
